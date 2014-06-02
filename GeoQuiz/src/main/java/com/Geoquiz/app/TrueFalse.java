d88bb07490cffa39eb305a57b0d40426e6e02808
Yu <yzheng04@syr.edu>
2014-06-02 14:05:36 -0400

    labs for all challenges in GeoQuiz

---
+++ GeoQuiz/src/main/java/com/Geoquiz/app/TrueFalse.java
@@ -0,0 +1,34 @@
+package com.Geoquiz.app;
+
+/**
+ * Created by koray on 5/30/14.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion){
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+
+
+    }
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
+
+}
